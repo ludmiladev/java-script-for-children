c5c8d7c99c9908a94bfb5aba405714a2b1dc6bf7
ludmiladev <ljuda12@gmail.com>
2021-03-14 16:23:58 +0200

    змінюємо висоту шрифта

---
+++ js/children.js
@@ -0,0 +1,21 @@
+// let greeting = "Hello";
+// greeting.length;
+
+// let a = "long stringtring";
+// "long stringtring".toUpperCase();
+// соnsole.log("long stringtring".toUpperCase());
+
+// let b = 3;
+// console.log(b);
+
+let stillyString = "heLlo THERE, hOW";
+let a = stillyString[0].toUpperCase() + stillyString.slice(1).toLowerCase();
+console.log(a);
+
+
+
+
+
+
+
+
