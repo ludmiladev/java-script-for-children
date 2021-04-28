42917a99583703dfd090e1fcf90926efb72e62bf
Ludmila Mychta <ljuda12@gmail.com>
2021-04-28 15:35:05 +0300

    створюэмо масиви

--- js/children.js
+++ js/children.js
@@ -1,17 +1,28 @@
 // let greeting = "Hello";
 // greeting.length;
 
+
 // let a = "long stringtring";
 // "long stringtring".toUpperCase();
 // соnsole.log("long stringtring".toUpperCase());
 
-// let b = 3;
+// let b = 5;
 // console.log(b);
 
-let stillyString = "heLlo THERE, hOW";
-let a = stillyString[0].toUpperCase() + stillyString.slice(1).toLowerCase();
-console.log(a);
 
+// let stillyString = "heLlo THERE, hOW";
+// let a = stillyString[0].toUpperCase() + stillyString.slice(1).toLowerCase();
+// console.log(a);
+
+// let num = 1 + 3;
+// let jgh = 8;
+// console.log(num / jgh);
+
+// ============ массиви================================================
+
+let x = 10;
+x = x+5
+x
 
 
 
