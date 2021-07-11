292c19d7b636576ecc4ca704879ca86e62ae76c8
Ludmila Mychta <ljuda12@gmail.com>
2021-07-11 20:57:06 +0300

    посчитать сумму цыфр числа через фор

--- js/children.js
+++ js/children.js
@@ -3550,3 +3550,12 @@
 //     break;
 // }
 // console.log(result);
+// ===========================================================================
+let userInput = prompt('Введите число');
+userInput = Number(userInput);
+let total = 0;
+
+for (let i=1; i<=userInput; i+=1) {
+  total +=i;
+}
+console.log(`Total = ${total}`);
